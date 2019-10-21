3c5e06eea75a6eda7414d37ce292e803d8ca6c6d
LuscapLudovic <kiluhasenpai@gmail.com>
2019-10-21 13:40:31 +0200

    redirection vers questionnaire

---
+++ src/views/Questionnaire.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        composants questionnaire
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Questionnaire"
+    }
+</script>
+
+<style scoped>
+
+</style>
